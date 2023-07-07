9718da7f2fb759ed491263e114b1c6694e331934
chipson <chipson>
2023-07-07 18:02:52 +0300

    Added bezierCurve, added pathfinding

--- Assets/Scripts/Game/BuildingManager.cs
+++ Assets/Scripts/Game/BuildingManager.cs
@@ -2,18 +2,27 @@ using System;
 using System.Collections.Generic;
 using BuildingNS;
 using Signals.Building;
+using Signals.UI;
+using Util;
+using Zenject;
 
 namespace Game
 {
-    public class BuildingManager 
+    public class BuildingManager
     {
+
+        [Inject] private Configuration _config;
+        
         private readonly BuildingSignals _buildingSignals;
+        private readonly UiSignals _uiSignals;
 
         private Dictionary<string, PlaceableBuilding> _placedBuildings;
-        
+        private List<int> _builtUniqueBuildings;
+
         public BuildingManager(BuildingSignals buildingSignals)
         {
             _placedBuildings = new Dictionary<string, PlaceableBuilding>();
+            _builtUniqueBuildings = new List<int>();
             _buildingSignals = buildingSignals;
             SubscribeOnBuildingPlacedEvent();
         }
@@ -29,17 +38,28 @@ namespace Game
         {
             return _placedBuildings;
         }
+
+        public List<int> GetBuiltUniqueBuildingList()
+        {
+            return _builtUniqueBuildings;
+        }
         
         private void SubscribeOnBuildingPlacedEvent()
         {
-            _buildingSignals.Subscribe2<BuildingPlacedSignal, Dictionary<string, PlaceableBuilding>>( (x, b) =>
-                BuildingPlaced(x, b)
-                , _placedBuildings);
+            _buildingSignals.Subscribe3<BuildingPlacedSignal, Dictionary<string, PlaceableBuilding>, List<int>>( (x, b, l) =>
+                BuildingPlaced(x, b, l)
+                , _placedBuildings, _builtUniqueBuildings);
         }
         
-        private Action<BuildingPlacedSignal, Dictionary<string, PlaceableBuilding>> BuildingPlaced = (s, d) =>
+        private Action<BuildingPlacedSignal, Dictionary<string, PlaceableBuilding>, List<int>> BuildingPlaced = (s, d, l) =>
         {
+            int configId = s.placeableBuilding.GetBuildingConfig().id;
             d.Add(s.placeableBuilding.GetId(), s.placeableBuilding);
+            if (!l.Contains(configId))
+            {
+                l.Add(configId);
+            }
         };
+        
     }
 }
--- Assets/Scripts/Game/ObjectPlacement.cs
+++ Assets/Scripts/Game/ObjectPlacement.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using BuildingNS;
+using Constant;
 using Signals.Building;
 using JetBrains.Annotations;
+using ModestTree;
 using Signals.UI;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Splines;
 using Util;
 using Zenject;
 
@@ -18,6 +23,7 @@ namespace Game
         [Inject] private readonly Configuration conf;
         [Inject] private readonly UiSignals _uiSignals;
         [Inject] private readonly BuildingSignals _buildingSignals;
+        [Inject] private readonly BuildingManager _buildingManager;
         
         [Inject] private readonly PlaceableBuildingFactory _placeableBuildingFactory;
         
@@ -27,6 +33,7 @@ namespace Game
 
         private GameObject _currentPlaceableObject = null;
         private PlaceableBuilding _currentPlaceableBuilding = null;
+        private PlaceableBuilding _currentPlaceableAnchor = null;
 
         private bool _objectInPlacement = false;
         private Ray _ray;
@@ -36,9 +43,17 @@ namespace Game
 
         private GameObject _hierarchyParent;
 
-        
+        private SplineContainer _splineContainer;
+        private LineRenderer _lineRenderer;
+        private NavMeshPath _navMeshPathHelper;
+        private bool _navMeshPathComplete = false;
         void Start()
         {
+            _splineContainer = gameObject.AddComponent<SplineContainer>();
+            _lineRenderer = gameObject.AddComponent<LineRenderer>();
+            _lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+            _navMeshPathHelper = new NavMeshPath();
+            
             _hierarchyParent = GameObject.Find("Objects"); // hack, move to own static class
             _prefabs = conf.GetCfgBuildingList();
             var firstBuildingConf = _prefabs.First();
@@ -47,7 +62,8 @@ namespace Game
             _selectedObject = new Building(fromResources, firstBuildingConf);
             SubscribeToUiSignals();
         }
-
+        
+        // todo: should be connected to timer?
         void Update()
         {
             if (_objectInPlacement && _currentPlaceableObject != null)
@@ -73,6 +89,14 @@ namespace Game
             var position = _currentPlaceableObject.transform.position;
             position.Set(position.x, 1f, position.z);
             _currentPlaceableObject.transform.position = position;
+            if (_currentPlaceableBuilding.GetBuildingConfig().id == BuildingConstants.PALACE_ID)
+            {
+                _navMeshPathComplete = true;
+            }
+            else
+            {
+                BuildBezier();
+            }
         }
 
         private void Placement()
@@ -96,7 +120,7 @@ namespace Game
                     var pointX = point.x;
                     var pointZ = point.z;
                     
-                    Debug.Log(scale.magnitude);
+                    //Debug.Log(scale.magnitude);
 
 
                     int offsetX = (int)((pointX - currentPos.x) / scale.x);
@@ -112,8 +136,8 @@ namespace Game
                 }
                 else
                 {
-                    Debug.Log(scale.magnitude);
-                    Debug.Log(scale.normalized);
+                    //Debug.Log(scale.magnitude);
+                    //Debug.Log(scale.normalized);
                     pos = new Vector3(
                         (currentPos.x),
                         ((currentPos.y) + 1) ,
@@ -121,21 +145,29 @@ namespace Game
                 }
 
                 _currentPlaceableObject.transform.position = pos;
+                
+                // TODO in case of multiple palaces anchor should be recalculated
+                if (_currentPlaceableAnchor != null)
+                {
+                    Curve(_currentPlaceableAnchor.transform, _currentPlaceableBuilding.transform);
+                }
             }
             else
             {
-                Debug.Log("Out of raycast");
+                //Debug.Log("Out of raycast");
             }
             
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 Destroy(_currentPlaceableObject);
                 _objectInPlacement = false;
+                _currentPlaceableAnchor = null;
+                DisableLineRenderer();
             }
 
             if (Input.GetMouseButtonDown(0))
             {
-                if (_currentPlaceableBuilding.canBuild)
+                if (_currentPlaceableBuilding.canBuild && _navMeshPathComplete)
                 {
                     BuildingPlacedSignal buildingPlacedSignal = new BuildingPlacedSignal
                     {
@@ -150,8 +182,123 @@ namespace Game
                     _objectInPlacement = false;
                     _currentPlaceableObject = null;
                     _currentPlaceableBuilding = null;
+                    _currentPlaceableAnchor = null;
+                    DisableLineRenderer();
                 }
             }
+        }
+
+        private void BuildBezier()
+        {
+                        
+            if (!_buildingManager.GetAllBuildings().Values.IsEmpty())
+            {
+                Vector3 buildingPos = _currentPlaceableBuilding.transform.position;
+                PlaceableBuilding[] anchors = 
+                    _buildingManager.GetAllBuildings().Values.
+                        Where(x => x.GetBuildingConfig().id == 4).ToArray();
+                float distance = 0;
+                foreach (var anchor in anchors)
+                {
+                    float tempDistance = Vector3.Distance(buildingPos, anchor.transform.position);
+                    if (distance == 0)
+                    {
+                        distance = tempDistance;
+                        _currentPlaceableAnchor = anchor;
+                    } else if (tempDistance < distance)
+                    {
+                        distance = tempDistance;
+                        _currentPlaceableAnchor = anchor;
+                    }
+                }
+            }
+
+            if (_currentPlaceableAnchor != null)
+            {
+                EnableLineRenderer();
+                Curve(_currentPlaceableAnchor.transform, _currentPlaceableBuilding.transform);
+            }
+        }
+
+        private void Curve(Transform initPos, Transform buildingPos)
+        {
+            if (_lineRenderer.enabled)
+            {
+                // Create a new Spline on the SplineContainer.
+                Spline spline = null; 
+                if (_splineContainer.Splines.Count == 0)
+                {
+                    spline = _splineContainer.AddSpline();
+                }
+                else
+                {
+                    spline = _splineContainer.Spline;
+                }
+                
+
+                // Set some knot values.
+                var knots = new BezierKnot[2];
+                knots[0] = new BezierKnot(new float3(initPos.position));
+                knots[1] = new BezierKnot(new float3(buildingPos.position));
+
+                knots[0].TangentOut = new float3(0f, 5f, 0f);
+                knots[1].TangentIn = new float3(0f, 5f, 0f);
+                spline.Knots = knots;
+
+                List<Vector3> p = new List<Vector3>();
+
+                var curve = spline.GetCurve(0);
+
+                for (float i = 0; i < 1f; i += 0.05f)
+                {
+                    var ab = Vector3.Lerp(curve.P0, curve.P1, i);
+                    var bc = Vector3.Lerp(curve.P1, curve.P2, i);
+                    var cd = Vector3.Lerp(curve.P2, curve.P3, i);
+
+                    var abbc = Vector3.Lerp(ab, bc, i);
+                    var bccd = Vector3.Lerp(bc, cd, i);
+
+                    var abcd = Vector3.Lerp(abbc, bccd, i);
+                    p.Add(abcd);
+                }
+                
+                spline.Clear();
+                
+                _lineRenderer.positionCount = p.Count;
+                _lineRenderer.startWidth = 0.1f;
+                _lineRenderer.endWidth = 0.1f;
+                _lineRenderer.numCornerVertices = 40;
+                _lineRenderer.SetPositions(p.ToArray());
+                _lineRenderer.loop = false;
+
+
+                // MASK == 1 == WALKABLE
+                NavMesh.CalculatePath(initPos.position, buildingPos.position, 1, _navMeshPathHelper);
+                _navMeshPathComplete = _navMeshPathHelper.status == NavMeshPathStatus.PathComplete;
+
+
+                
+                if (_currentPlaceableBuilding.canBuild && _navMeshPathComplete)
+                {
+                    _lineRenderer.startColor = Color.green;
+                    _lineRenderer.endColor = Color.green;
+                }
+                else
+                {
+                    _lineRenderer.startColor = Color.red;
+                    _lineRenderer.endColor = Color.red;
+                }
+            }
+        }
+        
+        private void DisableLineRenderer()
+        {
+            _lineRenderer.enabled = false;
+        }
+
+        private void EnableLineRenderer()
+        {
+            _lineRenderer.enabled = true;
         }
 
         // Subscribes
